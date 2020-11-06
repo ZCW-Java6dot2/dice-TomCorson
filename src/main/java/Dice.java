71f397088f477bb853795d6d651fcf3570b80a2c
TomCorson <corsontom@yahoo.com>
2020-11-05 21:57:48 -0500

    tests not working

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,12 @@
 public class Dice {
+   private int roll;
 
+    public  Dice(int amountOfDie){
+        this.roll=(int)(amountOfDie*(Math.random()*6+1));
+    }
+    public int getRoll(){
+        return this.roll;
+
+    }
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,10 @@
 public class Simulation {
 
+    public Simulation() {
+    }
 
-
+    public int tester() {
+        Dice test = new Dice(1);
+        return test.getDiceNum;
+    }
 }
